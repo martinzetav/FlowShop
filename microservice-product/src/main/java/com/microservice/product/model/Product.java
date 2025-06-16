b788c87966a0d36cbd8fa619d7aca54e432b76cb
Martin Zalazar <martinzetav@gmail.com>
2025-06-15 21:52:40 -0300

    se creo el model de products

---
+++ microservice-product/src/main/java/com/microservice/product/model/Product.java
@@ -0,0 +1,25 @@
+package com.microservice.product.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private Double price;
+    private Integer stock;
+
+
+
+
+}
