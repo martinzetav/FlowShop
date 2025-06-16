c36958e8a4c20f5de92cfbe59384a44e0fff51c3
Martin Zalazar <martinzetav@gmail.com>
2025-06-16 13:22:21 -0300

    se creo el controller de cart

---
+++ microservice-checkout/src/main/java/com/microservice/checkout/controller/CartController.java
@@ -0,0 +1,54 @@
+package com.microservice.checkout.controller;
+
+import com.microservice.checkout.dto.CartDTO;
+import com.microservice.checkout.exception.ResourceNotFoundException;
+import com.microservice.checkout.model.Cart;
+import com.microservice.checkout.model.CartItem;
+import com.microservice.checkout.service.ICartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/carts")
+public class CartController {
+
+    private final ICartService cartService;
+
+    @PostMapping
+    public ResponseEntity<CartDTO> save(@RequestBody Cart cart){
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartService.save(cart));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartDTO>> findAll(){
+        return ResponseEntity.ok(cartService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CartDTO> findById(@PathVariable Long id) throws ResourceNotFoundException {
+        return ResponseEntity.ok(cartService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CartDTO> update(@PathVariable Long id, @RequestBody Cart cart) throws ResourceNotFoundException {
+        return ResponseEntity.ok(cartService.update(id, cart));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) throws ResourceNotFoundException {
+        cartService.delete(id);
+        return ResponseEntity.ok("Cart with id " + id + " successfully removed.");
+    }
+
+    @PostMapping("/{id}/items")
+    public ResponseEntity<CartDTO> addItemToCart(@PathVariable Long id, @RequestBody CartItem cartItem) throws ResourceNotFoundException {
+        return ResponseEntity.ok(cartService.addOrUpdateItem(id, cartItem));
+    }
+
+
+}
