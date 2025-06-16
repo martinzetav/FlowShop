f3d611da8db6a0aa2190c25a7e1ceb38ce35b74f
Martin Zalazar <martinzetav@gmail.com>
2025-06-16 13:21:04 -0300

    se creo el repository de cart

---
+++ microservice-checkout/src/main/java/com/microservice/checkout/repository/ICartRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.checkout.repository;
+
+import com.microservice.checkout.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICartRepository extends JpaRepository<Cart, Long> {
+}
