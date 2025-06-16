de3ec3839d56cc92f47f8531155be484aab315ab
Martin Zalazar <martinzetav@gmail.com>
2025-06-15 21:52:23 -0300

    se creo el repository de products

---
+++ microservice-product/src/main/java/com/microservice/product/repository/IProductRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.product.repository;
+
+import com.microservice.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends JpaRepository<Product, Long> {
+}
