71c66a6ec3928b6a8ec60b339c1f3fa8f2fee406
Martin Zalazar <martinzetav@gmail.com>
2025-06-20 20:59:02 -0300

    se agrego la anotacion de feign client

--- microservice-order/src/main/java/com/microservice/order/MicroserviceOrderApplication.java
+++ microservice-order/src/main/java/com/microservice/order/MicroserviceOrderApplication.java
@@ -2,8 +2,12 @@ package com.microservice.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MicroserviceOrderApplication {
 
 	public static void main(String[] args) {
