0fc6b7f187f88511070b65d7c3522da03b46761b
Martin Zalazar <martinzetav@gmail.com>
2025-06-20 20:54:42 -0300

    se creo el repository del microservicio de order

---
+++ microservice-order/src/main/java/com/microservice/order/repository/IOrderRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.order.repository;
+
+import com.microservice.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IOrderRepository extends JpaRepository<Order, Long> {
+}
