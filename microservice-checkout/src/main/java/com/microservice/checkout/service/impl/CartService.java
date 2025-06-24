d05385b067c9a52e78f4d4360b9f564d6e69c940
Martin Zalazar <martinzetav@gmail.com>
2025-06-24 10:09:13 -0300

    se capturan las excepciones de los demas microservicios

--- microservice-checkout/src/main/java/com/microservice/checkout/service/impl/CartService.java
+++ microservice-checkout/src/main/java/com/microservice/checkout/service/impl/CartService.java
@@ -1,6 +1,8 @@
 package com.microservice.checkout.service.impl;
 
 import com.microservice.checkout.dto.CartDTO;
+import com.microservice.checkout.dto.ProductDTO;
+import com.microservice.checkout.exception.InsufficientStockException;
 import com.microservice.checkout.exception.ResourceAlreadyExistsException;
 import com.microservice.checkout.exception.ResourceNotFoundException;
 import com.microservice.checkout.mapper.CartMapper;
@@ -8,6 +10,8 @@ import com.microservice.checkout.model.Cart;
 import com.microservice.checkout.model.CartItem;
 import com.microservice.checkout.repository.ICartRepository;
 import com.microservice.checkout.service.ICartService;
+import com.microservice.checkout.service.IProductService;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +24,11 @@ public class CartService implements ICartService {
 
     private final ICartRepository cartRepository;
     private final CartMapper cartMapper;
+    private final IProductService productService;
 
     @Override
     public CartDTO save(Cart cart) {
+        // agregar validacion mediante USER_ID
         Cart savedCart = cartRepository.save(cart);
         return cartMapper.toDto(savedCart);
     }
@@ -73,6 +79,17 @@ public class CartService implements ICartService {
             throw new ResourceAlreadyExistsException("Product already exists in cart.");
         }
 
+        try{
+            ProductDTO product = productService.findById(newItem.getProductId());
+
+            if(newItem.getQuantity() > product.stock()){
+                throw new InsufficientStockException("Insufficient stock for product ID: " + product.id());
+            }
+
+        } catch (FeignException.NotFound e){
+            throw new ResourceNotFoundException("Product with id " + newItem.getProductId() + " not found.");
+        }
+
         items.add(newItem);
         Cart cartWithNewItem = cartRepository.save(cart);
         return cartMapper.toDto(cartWithNewItem);
@@ -89,7 +106,18 @@ public class CartService implements ICartService {
                 .findFirst()
                 .orElseThrow(() -> new ResourceNotFoundException("Item with id " + itemId + " not found in cart."));
 
-        existingItem.setQuantity(updatedItem.getQuantity());
+        try{
+            ProductDTO product = productService.findById(existingItem.getProductId());
+
+            if(updatedItem.getQuantity() > product.stock()){
+                throw new InsufficientStockException("Insufficient stock for product ID: " + product.id());
+            }
+
+            existingItem.setQuantity(updatedItem.getQuantity());
+
+        } catch (FeignException.NotFound e){
+            throw new ResourceNotFoundException("Product with id " + existingItem.getProductId() + " not found.");
+        }
 
         Cart updatedCart = cartRepository.save(cart);
         return cartMapper.toDto(updatedCart);
