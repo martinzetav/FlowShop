8788ad8803e7f2dee13fa0d6651c19e441fb2f9c
Martin Zalazar <martinzetav@gmail.com>
2025-06-24 17:39:10 -0300

    las excepciones ahora heredan de RuntimeException

--- microservice-checkout/src/main/java/com/microservice/checkout/exception/ResourceAlreadyExistsException.java
+++ microservice-checkout/src/main/java/com/microservice/checkout/exception/ResourceAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package com.microservice.checkout.exception;
 
-public class ResourceAlreadyExistsException extends Exception{
+public class ResourceAlreadyExistsException extends RuntimeException{
     public ResourceAlreadyExistsException(String message) {
         super(message);
     }
