aae9bb6f72acf8cf784295c3af51a45b02698350
Martin Zalazar <martinzetav@gmail.com>
2025-06-24 10:21:14 -0300

    se actualizo el endpoint para crear una orden

--- microservice-order/src/main/java/com/microservice/order/controller/OrderController.java
+++ microservice-order/src/main/java/com/microservice/order/controller/OrderController.java
@@ -1,15 +1,11 @@
 package com.microservice.order.controller;
 
 import com.microservice.order.dto.OrderDTO;
-import com.microservice.order.dto.request.OrderRequestDTO;
 import com.microservice.order.service.IOrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
@@ -18,9 +14,9 @@ public class OrderController {
 
     private final IOrderService orderService;
 
-    @PostMapping
-    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderRequestDTO orderRequestDTO){
-        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(orderRequestDTO));
+    @PostMapping("/{id}")
+    public ResponseEntity<OrderDTO> createOrder(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(id));
     }
 
 }
