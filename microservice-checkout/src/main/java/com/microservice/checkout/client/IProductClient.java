2a0a645ef91cb1e0bc644c874a4a7cb971970926
Martin Zalazar <martinzetav@gmail.com>
2025-06-24 10:16:37 -0300

    se creo el cliente del microservicio de productos

---
+++ microservice-checkout/src/main/java/com/microservice/checkout/client/IProductClient.java
@@ -0,0 +1,14 @@
+package com.microservice.checkout.client;
+
+import com.microservice.checkout.dto.ProductDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "msvc-product")
+public interface IProductClient {
+
+    @GetMapping("/products/{id}")
+    ProductDTO getProductById(@PathVariable Long id);
+
+}
