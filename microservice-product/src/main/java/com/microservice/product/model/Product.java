553bec5a75cd2caccab07877f005b38063c8456b
Martin Zalazar <martinzetav@gmail.com>
2025-06-26 12:26:53 -0300

    se agregaron restricciones al model Product

--- microservice-product/src/main/java/com/microservice/product/model/Product.java
+++ microservice-product/src/main/java/com/microservice/product/model/Product.java
@@ -13,10 +13,15 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 100)
     private String name;
+    @Column(nullable = false, length = 50)
     private String brand;
+    @Column(nullable = false, length = 200)
     private String description;
+    @Column(nullable = false)
     private Double price;
+    @Column(nullable = false)
     private Integer stock;
 
 
