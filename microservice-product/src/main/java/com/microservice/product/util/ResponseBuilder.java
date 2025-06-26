1ebf39616b9c29c8028d486cf59d3b635cd3c704
Martin Zalazar <martinzetav@gmail.com>
2025-06-26 12:25:30 -0300

    se creo un builder para las respuestas de las api

---
+++ microservice-product/src/main/java/com/microservice/product/util/ResponseBuilder.java
@@ -0,0 +1,58 @@
+package com.microservice.product.util;
+
+import com.microservice.product.api.response.ApiErrorResponse;
+import com.microservice.product.api.response.ApiSuccessResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+public class ResponseBuilder {
+
+    public static <T> ApiSuccessResponse<T> buildSuccessResponse(int status,
+                                                                 String message,
+                                                                 T data,
+                                                                 HttpServletRequest request)
+    {
+        return new ApiSuccessResponse<>(
+                status,
+                message,
+                data,
+                LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                request.getRequestURI()
+        );
+    }
+
+    public static ApiErrorResponse buildErrorResponse(int status,
+                                                      String error,
+                                                      String message,
+                                                      HttpServletRequest request
+    ) {
+        return new ApiErrorResponse(
+                status,
+                error,
+                message,
+                request.getRequestURI(),
+                LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                null
+        );
+    }
+
+    public static ApiErrorResponse buildValidationErrorResponse(
+            HttpServletRequest request,
+            Map<String, String> fieldErrors
+    ) {
+        return new ApiErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "Validation errors in form fields",
+                request.getRequestURI(),
+                LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                fieldErrors
+        );
+    }
+
+
+}
