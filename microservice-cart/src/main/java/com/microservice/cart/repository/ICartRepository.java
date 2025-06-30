0e98b732ea75c4d18c8634fba91963bab534a9e9
Martin Zalazar <martinzetav@gmail.com>
2025-06-29 22:26:31 -0300

    se implemento consulta derivada para traer los carritos con usuarios activos

--- microservice-cart/src/main/java/com/microservice/cart/repository/ICartRepository.java
+++ microservice-cart/src/main/java/com/microservice/cart/repository/ICartRepository.java
@@ -1,9 +1,13 @@
 package com.microservice.cart.repository;
 
 import com.microservice.cart.model.Cart;
+import com.microservice.cart.model.CartStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ICartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserIdAndStatus(Long userId, CartStatus status);
 }
