6193a96b9d99b9c15b10ec0fa3ff670eea4cddb1
Martin Zalazar <martinzetav@gmail.com>
2025-07-01 11:39:30 -0300

    se mejoro la respuesta de errores

--- microservice-order/src/main/java/com/microservice/order/exception/GlobalExceptionHandler.java
+++ microservice-order/src/main/java/com/microservice/order/exception/GlobalExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.microservice.order.exception;
 
+import com.flowshop.common.api.response.ApiErrorResponse;
+import com.flowshop.common.exception.InsufficientStockException;
+import com.flowshop.common.exception.ResourceNotFoundException;
+import com.flowshop.common.util.ResponseBuilder;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +14,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InsufficientStockException.class)
-    public ResponseEntity<String> processInsufficientStockException(InsufficientStockException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    public ResponseEntity<ApiErrorResponse> processInsufficientStockException(InsufficientStockException e,
+                                                                              HttpServletRequest request){
+        ApiErrorResponse error = ResponseBuilder.buildErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                e.getMessage(),
+                request
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> processResourceNotFoundException(ResourceNotFoundException e){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<ApiErrorResponse> processResourceNotFoundException(ResourceNotFoundException e,
+                                                                             HttpServletRequest request){
+        ApiErrorResponse error = ResponseBuilder.buildErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                e.getMessage(),
+                request
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
 }
