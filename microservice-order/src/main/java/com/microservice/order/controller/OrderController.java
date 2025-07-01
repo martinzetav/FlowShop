eaa253945e256e876e469cb34a1581334193627a
Martin Zalazar <martinzetav@gmail.com>
2025-07-01 11:39:48 -0300

    se mejoro la respuesta al crear una orden

--- microservice-order/src/main/java/com/microservice/order/controller/OrderController.java
+++ microservice-order/src/main/java/com/microservice/order/controller/OrderController.java
@@ -1,11 +1,15 @@
 package com.microservice.order.controller;
 
-import com.microservice.order.dto.OrderDTO;
+import com.flowshop.common.api.response.ApiSuccessResponse;
+import com.flowshop.common.util.ResponseBuilder;
+import com.microservice.order.dto.response.OrderResponseDTO;
 import com.microservice.order.service.IOrderService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/orders")
@@ -15,8 +19,19 @@ public class OrderController {
     private final IOrderService orderService;
 
     @PostMapping("/{id}")
-    public ResponseEntity<OrderDTO> createOrder(@PathVariable Long id){
-        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(id));
+    public ResponseEntity<ApiSuccessResponse<OrderResponseDTO>> createOrder(@PathVariable Long id,
+                                                                            HttpServletRequest request,
+                                                                            UriComponentsBuilder uriComponentsBuilder){
+        OrderResponseDTO savedOrder = orderService.createOrder(id);
+        var uri = uriComponentsBuilder.path("/orders/{id}").buildAndExpand(savedOrder.id()).toUri();
+
+        ApiSuccessResponse<OrderResponseDTO> response = ResponseBuilder.buildSuccessResponse(
+                HttpStatus.CREATED.value(),
+                "Order created successfully",
+                savedOrder,
+                request
+        );
+        return ResponseEntity.created(uri).body(response);
     }
 
 }
