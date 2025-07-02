a36a6601ce4a3fb9c6c33c594392b11d3354c7f3
Martin Zalazar <martinzetav@gmail.com>
2025-07-01 23:14:30 -0300

    se cambio el metodo http

--- microservice-order/src/main/java/com/microservice/order/client/ICartClient.java
+++ microservice-order/src/main/java/com/microservice/order/client/ICartClient.java
@@ -3,8 +3,8 @@ package com.microservice.order.client;
 import com.microservice.order.dto.CartDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @FeignClient(name = "msvc-cart")
 public interface ICartClient {
@@ -12,7 +12,7 @@ public interface ICartClient {
     @GetMapping("/carts/internal/{id}")
     CartDTO getCartById(@PathVariable Long id);
 
-    @PatchMapping("/carts/internal/{id}/complete")
+    @PutMapping("/carts/internal/{id}/complete")
     void completeCart(@PathVariable Long id);
 
 }
