a39fe5b5266dd3eef250d10f63854b2da1c635be
Martin Zalazar <martinzetav@gmail.com>
2025-07-01 21:30:27 -0300

    se creo consulta derivada para traer todas las ordenes de un usuario

--- microservice-order/src/main/java/com/microservice/order/repository/IOrderRepository.java
+++ microservice-order/src/main/java/com/microservice/order/repository/IOrderRepository.java
@@ -1,9 +1,12 @@
 package com.microservice.order.repository;
 
 import com.microservice.order.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IOrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findAllByUserId(Long userId, Pageable pageable);
 }
