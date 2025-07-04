c8f9817ef511b304ac2a291a60cdb35f2f04036b
Martin Zalazar <martinzetav@gmail.com>
2025-07-03 22:42:32 -0300

    se cambio la config de circuit breaker al repo

--- microservice-cart/src/main/java/com/microservice/cart/exception/ProductServiceUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.cart.exception;
-
-public class ProductServiceUnavailableException extends RuntimeException{
-    public ProductServiceUnavailableException(String message) {
-        super(message);
-    }
-}
