ce7af3311b7bfa88fc6d4ca297d90001494d6d03
Martin Zalazar <martinzetav@gmail.com>
2025-07-19 11:16:01 -0300

    se implemento circuit breaker

--- microservice-cart/src/main/java/com/microservice/cart/service/impl/UserService.java
+++ microservice-cart/src/main/java/com/microservice/cart/service/impl/UserService.java
@@ -1,8 +1,12 @@
 package com.microservice.cart.service.impl;
 
+import com.flowshop.common.exception.UserServiceUnavailableException;
 import com.microservice.cart.client.IUserClient;
 import com.microservice.cart.dto.UserDTO;
 import com.microservice.cart.service.IUserService;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +17,15 @@ public class UserService implements IUserService {
     private final IUserClient userClient;
 
     @Override
+    @CircuitBreaker(name = "user", fallbackMethod = "fallbackFindUserById")
+    @Retry(name = "user")
     public UserDTO findUserById(Long id) {
         return userClient.getUserById(id);
+    }
+
+    public UserDTO fallbackFindUserById(Long id, CallNotPermittedException e){
+        throw new UserServiceUnavailableException(
+                "User service is currently unavailable. Please try again later."
+        );
     }
 }
